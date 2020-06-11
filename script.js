1223ca891640785ffb03ef7588c7d42d2c6c60ed
SymPiracha <sym.piracha@gmail.com>
2020-06-11 14:06:03 +0500

    color picker issue fixed

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-import { without } from "lowdash";
-console.log(without);
+// import { without } from "lodash";
+// console.log(without);
 
-var css = document.querySelector("h3");
+var css = document.querySelector("h5");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient"); 
+ 
+ color1.addEventListener("input",setGradient);
+ color2.addEventListener("input",setGradient);
 
 function setGradient() {
 	body.style.background = 
@@ -13,8 +16,6 @@ function setGradient() {
 	+ ", " 
 	+ color2.value 
 	+ ")";
-	css.textContent = body.style.background + ";"; 
+	css.textContent = "Gradient: " + color1.value + " to " + color2.value;
 }
 
-color1.addEventListener("input",setGradient);
-color2.addEventListener("input",setGradient);
